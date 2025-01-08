361e4b13d5cea88c67386f10cc31c88d3ccab2cd
Balaiah <thupakulabalaiah352@gmail.com>
2025-01-08 21:41:44 +0530

    logic

---
+++ Demo/src/in/Demo/Logical.java
@@ -0,0 +1,15 @@
+package in.Demo;
+
+public class Logical {
+public static void main(String[] args) {
+	int a= 10;
+	int b=20;
+	int c=40;
+	if((a<b)&&(a==c)) {
+		System.out.println(true);
+	}
+	if((a>b)||(a!=b)) {
+		System.out.println(false);
+	}
+}
+}
